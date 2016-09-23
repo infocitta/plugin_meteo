90973a740ec3002489bf1d1e1080e9989dfea728
Paolo Fisco <paolo.fisco@gmail.com>
2016-09-23 14:45:12 +0200

    fix folder

---
+++ PluginMeteo/cForecast.vb
@@ -0,0 +1,41 @@
+﻿Namespace nForecast
+    Public Class Features
+        Public Property forecast As Integer
+    End Class
+
+    Public Class Response
+        Public Property version As String
+        Public Property termsofService As String
+        Public Property features As Features
+    End Class
+
+    Public Class Forecastday
+        Public Property period As Integer
+        Public Property icon As String
+        Public Property icon_url As String
+        Public Property title As String
+        Public Property fcttext As String
+        Public Property fcttext_metric As String
+        Public Property pop As String
+    End Class
+
+    Public Class TxtForecast
+        Public Property [date] As String
+        Public Property forecastday As List(Of Forecastday)
+    End Class
+    Public Class Simpleforecast
+        Public Property forecastday As List(Of Forecastday)
+    End Class
+
+    Public Class Forecast
+        Public Property txt_forecast As TxtForecast
+        Public Property simpleforecast As Simpleforecast
+    End Class
+
+    Public Class clsForecast
+        Public Property response As Response
+        Public Property forecast As Forecast
+    End Class
+
+
+End Namespace
---
+++ PluginMeteo/default.aspx.vb
@@ -0,0 +1,74 @@
+﻿Imports System.Net
+Imports Newtonsoft.Json
+
+Public Class _default
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        If Request.UserHostAddress <> My.Settings.Host Then Exit Sub
+        Try
+            Select Case Request.QueryString("TYPE").ToUpper
+                Case "CONDITIONS"
+                    Try
+                        Dim page As String
+                        Using w As New WebClient
+                            w.Encoding = Encoding.UTF8
+                            page = w.DownloadString("http://api.wunderground.com/api/" + My.Settings.Token + "/conditions/lang:IT/q/Italy/" + Request.QueryString("city") + ".json")
+                        End Using
+                        Dim result = JsonConvert.DeserializeObject(Of nConditions.clsCondition)(page)
+                        With result.current_observation
+                            Dim printresult As New StringBuilder
+                            printresult.AppendLine(String.Join("", Enumerable.Repeat(":wavy_dash:", 10)))
+                            printresult.AppendLine("Meteo " & .display_location.city)
+                            printresult.AppendLine("Situazione: " + .weather + " " + DecodeEmoji(.icon))
+                            printresult.AppendLine("Temperatura: " + .temp_c.ToString)
+                            printresult.AppendLine("Umidità: " + .relative_humidity)
+                            printresult.AppendLine(String.Join("", Enumerable.Repeat(":wavy_dash:", 10)))
+                            Response.Write(printresult.ToString)
+                        End With
+                    Catch ex As Exception
+
+                    End Try
+                Case "FORECAST"
+                    Try
+                        Dim page As String
+                        Using w As New WebClient
+                            w.Encoding = Encoding.UTF8
+                            page = w.DownloadString("http://api.wunderground.com/api/" + My.Settings.Token + "/forecast/lang:IT/q/Italy/" + Request.QueryString("city") + ".json")
+                        End Using
+                        Dim result = JsonConvert.DeserializeObject(Of nForecast.clsForecast)(page)
+                        With result.forecast.txt_forecast
+                            Dim printresult As New StringBuilder
+                            printresult.AppendLine(String.Join("", Enumerable.Repeat(":wavy_dash:", 10))).AppendLine 
+                            For Each fday In .forecastday
+                                printresult.AppendLine(":arrow_right: " + fday.title + " " + DecodeEmoji(fday.icon))
+                                printresult.AppendLine(fday.fcttext_metric).AppendLine()
+                            Next
+                            printresult.AppendLine(String.Join("", Enumerable.Repeat(":wavy_dash:", 10)))
+                            Response.Write(printresult.ToString)
+                        End With
+                    Catch ex As Exception
+
+                    End Try
+            End Select
+        Catch ex As Exception
+
+        End Try
+    End Sub
+    Function DecodeEmoji(sCode As String) As String
+        Select Case sCode
+            Case "clear", "sunny", "nt_clear"
+                Return ":sunny:"
+            Case "rain"
+                Return ":umbrella:"
+            Case "partlycloudy", "nt_partlycloudy"
+                Return ":partly_sunny:"
+            Case "mostlycloudy"
+                Return ":partly_sunny:"
+            Case "cloudy"
+                Return ":cloud:"
+            Case Else
+                Return ""
+        End Select
+    End Function
+End Class
